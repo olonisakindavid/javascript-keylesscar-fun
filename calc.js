28f10bedfa8252676f877c17fd6d605a8d3af6fe
Olonisakin David <66315270+olonisakindavid@users.noreply.github.com>
2023-03-18 21:25:46 +0100

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,36 @@
+// var  firstName = prompt("enter your first Number");
+// var  secondName = prompt("enter your last Number");
+// var sum = Number(firstName) + Number(secondName);
+// var sumOfNUmbers = alert("Sum of the answer = " + sum );
+
+// var name= "David";
+
+// if(name === "David"){
+// 	alert("Hi David");
+// }
+// else if(name =="Favour"){
+// 	alert("Hi Bot");
+// }
+// else{
+// 	alert( "i dont know you");
+// }
+
+// if(name === "David" || name = "Dara"){}
+// if(name === "David" && name = "Dara"){}
+// if(!(name==="David")){} .......do something
+
+alert("Welcome to your Tesla");
+checkDriverAge();
+function checkDriverAge(){;
+	age = prompt("How old are you?")
+	if (Number(age) < 18) {
+	alert("Sorry, you are too young to drive this car. Powering off");
+} else if (Number(age) > 18) {
+	alert("Powering On. Enjoy the ride!");
+} else if (Number(age) === 18) {
+	alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
+	console.log("Done programming bro");
+}
+
+
