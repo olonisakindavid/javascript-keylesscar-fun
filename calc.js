dabd3232fff3292df75cf733c73f028a680ac6f0
Olonisakin David <66315270+olonisakindavid@users.noreply.github.com>
2023-03-18 21:42:46 +0100

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,36 +1,52 @@
-// var  firstName = prompt("enter your first Number");
-// var  secondName = prompt("enter your last Number");
-// var sum = Number(firstName) + Number(secondName);
-// var sumOfNUmbers = alert("Sum of the answer = " + sum );
+// Make an keyless car EVEN BETTER!
+// We are improving our car from previous exercise now. 
 
-// var name= "David";
 
-// if(name === "David"){
-// 	alert("Hi David");
-// }
-// else if(name =="Favour"){
-// 	alert("Hi Bot");
-// }
-// else{
-// 	alert( "i dont know you");
-// }
+var age = prompt("What is your age?");
 
-// if(name === "David" || name = "Dara"){}
-// if(name === "David" && name = "Dara"){}
-// if(!(name==="David")){} .......do something
-
-alert("Welcome to your Tesla");
-checkDriverAge();
-function checkDriverAge(){;
-	age = prompt("How old are you?")
-	if (Number(age) < 18) {
-	alert("Sorry, you are too young to drive this car. Powering off");
+if (Number(age) < 18) {
+	alert("Sorry, you are too yound to drive this car. Powering off");
 } else if (Number(age) > 18) {
 	alert("Powering On. Enjoy the ride!");
 } else if (Number(age) === 18) {
 	alert("Congratulations on your first year of driving. Enjoy the ride!");
 }
-	console.log("Done programming bro");
+
+//1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
+function checkDriverAge() {
+	var age = prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+// Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+
+
+//2. Create another function that does the same thing, assign it to checkDriverAge2 variable using Function Expression.
+var checkDriverAge2 = function() {
+	var age = prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
 }
 
-
+//BONUS: Instead of using the prompt. Now, only use console.log and make the checkDriverAge() function accept an argument of age, so that if you enter:
+// checkDriverAge(92);
+// it returns "Powering On. Enjoy the ride!" 
+function checkDriverAge(age) {
+	if (Number(age) < 18) {
+		return "Sorry, you are too yound to drive this car. Powering off";
+	} else if (Number(age) > 18) {
+		return "Powering On. Enjoy the ride!";
+	} else if (Number(age) === 18) {
+		return "Congratulations on your first year of driving. Enjoy the ride!";
+	}
+}
